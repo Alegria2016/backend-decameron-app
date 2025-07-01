aca231c99dfd51e231f7947ded65e2c2f54be043
felix alegria <felix.alegria@bjungle.net>
2025-06-30 21:08:45 -0500

    check migraciones.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/check-migrations', function () {
+    try {
+        // Verifica conexión a la BD
+        DB::connection()->getPdo();
+        
+        // Ejecuta migrate:status y captura el output
+        Artisan::call('migrate:status');
+        $output = Artisan::output();
+        
+        return "<pre>Migraciones:\n" . $output . "</pre>";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
